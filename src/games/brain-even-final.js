c1a8ac47ef71c66fb8a268ac86381a7d685d8d0f
Георгий Лесниченко <zhoric9@gmail.com>
2021-11-30 19:06:55 +0300

    change brain-even-final

--- src/games/brain-even-final.js
+++ src/games/brain-even-final.js
@@ -4,9 +4,8 @@ import game from '../index.js';
 const parityNumber = (number) => number % 2 === 0;
 const getGameData = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  const number = getRandomNumber(1, 100);
-  const question = number;
-  const answer = parityNumber(number) ? 'yes' : 'no';
+  const question = getRandomNumber(1, 100);
+  const answer = parityNumber(question) ? 'yes' : 'no';
   return [question, String(answer)];
 };
 const brainEven = () => game(getGameData);
