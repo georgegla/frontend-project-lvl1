e3f8c40df52cc2cc41973e8b73c7b3f9e6482647
Георгий Лесниченко <zhoric9@gmail.com>
2021-11-30 23:47:43 +0300

    changed brain-calc

--- src/games/brain-calc-final.js
+++ src/games/brain-calc-final.js
@@ -1,17 +1,26 @@
 import game from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-const getRandomOper = () => {
-  const oper = ['-', '+', '*'];
-  return (oper[getRandomNumber(0, 2)]);
+const calculate = (number1, number2, oper) => {
+  switch (oper) {
+    case '-':
+      return number1 - number2;
+    case '+':
+      return number1 + number2;
+    case '*':
+      return number1 * number2;
+    default:
+      return 'Operator not found!';
+  }
 };
 const getGameData = () => {
-  const number1 = getRandomNumber(1, 25);
-  const number2 = getRandomNumber(1, 25);
-  const operation = getRandomOper();
+  const oper = ['-', '+', '*'];
+  const number1 = getRandomNumber(1, 10);
+  const number2 = getRandomNumber(1, 10);
+  const operation = oper[getRandomNumber(0, 2)];
   console.log('What is the result of the expression?');
   const question = `${number1} ${operation} ${number2}`;
-  const answer = eval(question);
+  const answer = calculate(number1, number2, operation);
   return [question, String(answer)];
 };
 const brainCalc = () => game(getGameData);
