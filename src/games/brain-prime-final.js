67d5e4c8b1d7c1365b1eb23c9a9b109b4aa5c716
Георгий Лесниченко <zhoric9@gmail.com>
2021-12-15 12:19:31 +0300

    changed brain-prime

--- src/games/brain-prime-final.js
+++ src/games/brain-prime-final.js
@@ -3,7 +3,7 @@ import getRandomNumber from '../utils.js';
 
 const isPrime = (number) => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < number; i += 1) {
+  for (let i = 2; i < number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
